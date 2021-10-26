35fd4a10c85f92d6870d001869efd85f957102b4
Berkhan Berkdemir <berkhan@tekdanisman.com>
2021-10-26 13:31:02 +0300

    Run ESLint with --fix flag

--- code/tests.js
+++ code/tests.js
@@ -1,51 +1,49 @@
-const Nano = require('./index.js')
+const Nano = require('./index.js');
 
-const debug = true
+const debug = true;
 
 function validate(name, value, type, output) {
-	if (value && (typeof value).toLowerCase() == (type || 'string').toLowerCase()) {
-		console.log( `${name}:`, '\x1b[32m', "Ok", '\x1b[0m')
-	} else {
-		console.log( `${name}:`, '\x1b[41m', "Error", '\x1b[0m')
-	}
-	if (debug && output) console.log(output)
+  if (value && (typeof value).toLowerCase() == (type || 'string').toLowerCase()) {
+    console.log(`${name}:`, '\x1b[32m', 'Ok', '\x1b[0m');
+  } else {
+    console.log(`${name}:`, '\x1b[41m', 'Error', '\x1b[0m');
+  }
+  if (debug && output) console.log(output);
 }
 
-;(async () => {
+(async () => {
+  const address = 'nano_37y6iq8m1zx9inwkkcgqh34kqsihzpjfwgp9jir8xpb9jrcwhkmoxpo61f4o'; // Nano.to
+  const address2 = 'nano_1xkyayfycettzcyhbexcmtqbhj5abbruq15urypzsnrby9s8nrhpj6fzxqcg'; // Random Address
 
-	var address = `nano_37y6iq8m1zx9inwkkcgqh34kqsihzpjfwgp9jir8xpb9jrcwhkmoxpo61f4o` // Nano.to
-	var address2 = `nano_1xkyayfycettzcyhbexcmtqbhj5abbruq15urypzsnrby9s8nrhpj6fzxqcg` // Random Address
+  console.clear();
 
-	console.clear()
+  const startTime = Date.now();
 
-	var startTime = Date.now();
+  // Price from Nano.to, CoinMarketCap cached for 60 seconds.
+  validate('Nano.price()', await Nano.price(), 'number');
 
-	// Price from Nano.to, CoinMarketCap cached for 60 seconds.
-	validate( "Nano.price()", await Nano.price(), 'number')
+  // Price from CoinMarketCap
+  // check( "Nano.price({ config })", (await api.price({ key: '{{ API_KEY }}' })).price, 'number')
+  const account = await Nano.account(address);
+  validate('Nano.account()', account.balance, 'string');
 
-	// Price from CoinMarketCap
-	// check( "Nano.price({ config })", (await api.price({ key: '{{ API_KEY }}' })).price, 'number')
-	var account = await Nano.account(address) 
-	validate( "Nano.account()", account.balance, 'string')
+  const pending = await Nano.pending(address);
+  validate('Nano.pending()', pending, 'object');
 
-	var pending = await Nano.pending(address) 
-	validate( "Nano.pending()", pending, 'object')
+  const history = await Nano.history(address2);
+  validate('Nano.address()', history, 'object');
 
-	var history = await Nano.history(address2) 
-	validate( "Nano.address()", history, 'object')
+  const rawAmount = '19424000000000000000000000000';
+  const convertFromRaw = Nano.fromRaw(rawAmount);
+  validate('Nano.fromRaw()', convertFromRaw == '0.019424', 'boolean');
 
-	var rawAmount = '19424000000000000000000000000'
-	var convertFromRaw =  Nano.fromRaw(rawAmount)
-	validate( "Nano.fromRaw()", convertFromRaw == '0.019424', 'boolean')
+  const convertToRaw = Nano.toRaw(convertFromRaw);
+  validate('Nano.toRaw()', convertToRaw == rawAmount, 'boolean');
 
-	var convertToRaw =  Nano.toRaw(convertFromRaw)
-	validate( "Nano.toRaw()", convertToRaw == rawAmount, 'boolean')
+  const findBlockByAmount = Nano.findBlockByAmount(address2, history[0].amount);
+  validate('Nano.findBlockByAmount()', findBlockByAmount, 'object');
 
-	var findBlockByAmount =  Nano.findBlockByAmount(address2, history[0].amount)
-	validate( "Nano.findBlockByAmount()", findBlockByAmount, 'object')
+  const elapsedTime = Date.now() - startTime;
 
-	var elapsedTime = Date.now() - startTime;
-
-	console.log("Finished in", `${elapsedTime}ms`)
-
-})()
+  console.log('Finished in', `${elapsedTime}ms`);
+})();
