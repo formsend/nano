3cabbf5ade95e8d754ac7a7fc3b7813cf1e4316d
Esteban <santiestebanesteban@gmail.com>
2021-11-01 01:46:39 -0400

    fix test

--- test/tests/getHistory.js
+++ test/tests/getHistory.js
@@ -1,10 +1,10 @@
 const axios = require("axios");
 
 axios.get("https://api.nano.to/history/nano_1bc8gx518sp55o6nmxw8n9hh9uco8mig9suy79s4mo59dsr7uz84gbfkb7w1").then(res=>{
-    if (res.data[0].hash == "29891E93560EDD01F1637C23569D63C4968FFA0AC500D2E595269D55F3159F5E") {
+    if (res.data[0] && typeof res.data[0].hash == "string") {
         console.log(`Get History:`, '\x1b[32m', "Ok", '\x1b[0m')
     } else {
-        console.log(`Get History:`, '\x1b[41m', "Error", '\x1b[0m')
+        console.log(`Get History:`, '\x1b[41m', "Error", '\x1b[0m', res.data[0].hash)
     }
 }).catch(error=>{
     console.log(`Get History:`, '\x1b[41m', "Error", '\x1b[0m')
