3252597d22e2e3d46646f21881683979bc628c4a
Esteban <santiestebanesteban@gmail.com>
2021-11-26 02:38:06 -0500

    here we go

--- test/tests/getQrCodeWithAmount.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-
-axios.get(`https://nano.to/qrcode/Moon/0.5683`).then(res => {
-    console.log(`Get QR Code With Amount:`, '\x1b[32m', "Ok", '\x1b[0m')
-}).catch(error => {
-    console.log(`Get QR Code With Amount:`, '\x1b[41m', "Error", '\x1b[0m')
-});
